21c82fe60c7eabd8cb5f8fd871fc26f40d7bfbfd
Lucria <jerryzhang28eclipse@gmail.com>
2020-04-09 12:03:36 +0800

    [Docs] Added documentation for the controller layer

--- Scooter/Controllers/ScooterController.cs
+++ Scooter/Controllers/ScooterController.cs
@@ -17,6 +17,7 @@ namespace Beam_intern.Scooter.Controllers
     [Route("scooters/")]
     public class ScooterController : ControllerBase
     {
+        // _logger is for future usages as logging is important in spotting bugs.
         private readonly ILogger<ScooterController> _logger;
         private readonly IScooterService _scooterService;
 
@@ -26,13 +27,29 @@ namespace Beam_intern.Scooter.Controllers
             _scooterService = scooterService;
         }
 
+        /**
+         * GET API for all scooters present in the database
+         * Returns either an error codee 404 if the operation failed
+         * Or a response code 200 with the details of all scooters in the database.
+         */
         [HttpGet]
         public async Task<ActionResult<IEnumerable<ScooterDomainModel>>> GetAll()
         {
             var result = await _scooterService.GetAll();
+            if (result == null)
+            {
+                return NotFound();
+            }
             return Ok(result.Select(ToScooterResponse));
         }
 
+        /**
+         * GET API for a specific scooter.
+         * One of the 4 main HTTP requests (GET, POST, PUT, DELETE)
+         * Takes in a UUID and returns either
+         * an error code 404 if the operation failed
+         * or a response code 200 and the details of the specific scooter if it can be found.
+         */
         [HttpGet("{id}")]
         public async Task<ActionResult<ScooterDomainModel>> Get(Guid id)
         {
@@ -45,18 +62,19 @@ namespace Beam_intern.Scooter.Controllers
             return Ok(ToScooterResponse(result));
         }
 
+        /**
+         * POST API for the main part of the project:
+         * Getting X nearest scooters within Y metres at a chosen latitude and longitude
+         * Takes in a new request model called ScooterClosestRequestModel
+         * that requires NearestNumberOfScooters, Radius and a ChosenLatitude and ChosenLongitude.
+         * Controller will call on the ScooterService to process the domain logic before returning
+         * either an error code 404 if operation is not completed successfully
+         * or an response code 200 and a List of ScooterResponseModel of the chosen Scooters. 
+         */
         [HttpPost("closest/")]
         public ActionResult<IEnumerable<ScooterClosestDomainModel>> GetClosest([FromBody] ScooterClosestRequestModel scooterClosestRequestModel)
-        // X number of scooters
-        // within Y metres radius
-        // At chosen latitude and longitude
         {
-            // Need change request model to a domain model
-            ScooterClosestDomainModel scooterClosestDomainModel = new ScooterClosestDomainModel(
-                scooterClosestRequestModel.NearestNumberOfScooters,
-                scooterClosestRequestModel.Radius,
-                scooterClosestRequestModel.ChosenLatitude,
-                scooterClosestRequestModel.ChosenLongitude);
+            ScooterClosestDomainModel scooterClosestDomainModel = ToScooterClosestDomainModel(scooterClosestRequestModel);
             var result =  _scooterService.GetClosest(scooterClosestDomainModel);
             if (result == null)
             {
@@ -66,6 +84,13 @@ namespace Beam_intern.Scooter.Controllers
             return Ok(result.Select(ToScooterResponse));
         }
 
+        /**
+         * POST API to create a new Scooter in the database with specific coordinates
+         * One of the 4 main HTTP requests (GET, POST, PUT, DELETE)
+         * Takes in a ScooterRequestModel, which requires new latitude and longitude.
+         * Returns either an error code 404 if operation is not completed correctly.
+         * Or returns response code 201 to indicate scooter object is created successfully.
+         */
         [HttpPost]
         public async Task<ActionResult<ScooterDomainModel>> Post([FromBody] ScooterRequestModel requestModel)
         {
@@ -79,6 +104,13 @@ namespace Beam_intern.Scooter.Controllers
             return Created(Request.Path.Value + "/" + result.Id, result);
         }
 
+        /**
+         * PUT API for a Scooter using a specific GUID and new Coordinates
+         * One of the 4 main HTTP requests (GET, POST, PUT, DELETE)
+         * Takes in a Guid id and a ScooterRequestModel. ScooterRequestModel requires new latitude and longitude.
+         * Returns either an error code 404 if desired scooter is not present
+         * Or returns response code 200 if desired scooter is present.
+         */
         [HttpPut("{id}")]
         public async Task<ActionResult<ScooterDomainModel>> Put(Guid id, [FromBody] ScooterRequestModel requestModel)
         {
@@ -91,8 +123,14 @@ namespace Beam_intern.Scooter.Controllers
             return Ok(ToScooterResponse(result));
         }
 
+        /**
+         * Delete API for a Scooter using Guid.
+         * One of the 4 main HTTP requests (GET, POST, PUT, DELETE)
+         * Takes in a Guid id and returns either an error when the desired scooter doesn't exist
+         * Or Response Code 204 to indicate that desired scooter has been deleted.
+         */
         [HttpDelete("{id}")]
-        public async Task<ActionResult<ScooterDomainModel>> Delete(Guid id)
+        public async Task<ActionResult> Delete(Guid id)
         {
             var result = await _scooterService.Delete(id);
             if (result == null)
@@ -103,12 +141,33 @@ namespace Beam_intern.Scooter.Controllers
             return NoContent();
         }
 
+        /**
+         * Method responsible for converting a Domain model to a Response model for the View layer
+         * As this project is a small scale one, ScooterResponseModel is the same as ScooterDomainModel.
+         * In larger scale projects, the Response model may be very different compared to the Domain model.
+         */
         private ScooterResponseModel ToScooterResponse (ScooterDomainModel scooterDomainModel)
         {
             return new ScooterResponseModel(
                 scooterDomainModel.Id, 
                 scooterDomainModel.Latitude, 
                 scooterDomainModel.Longitude);
+        }
+
+        /**
+         * Method responsible for converting a ClosestRequestModel to a ClosetDomainModel for
+         * domain logic in the Services.
+         * As this project is small scale, ClosestDomainModel is similar to ClosestRequestModel.
+         */
+        private ScooterClosestDomainModel ToScooterClosestDomainModel(
+            ScooterClosestRequestModel scooterClosestRequestModel)
+        {
+            ScooterClosestDomainModel scooterClosestDomainModel = new ScooterClosestDomainModel(
+                scooterClosestRequestModel.NearestNumberOfScooters,
+                scooterClosestRequestModel.Radius,
+                scooterClosestRequestModel.ChosenLatitude,
+                scooterClosestRequestModel.ChosenLongitude);
+            return scooterClosestDomainModel;
         }
     }
 }
