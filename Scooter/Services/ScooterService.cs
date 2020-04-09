febfaac6dbe45d4c34695fc2b54e7ec73aa1ee53
Lucria <jerryzhang28eclipse@gmail.com>
2020-04-09 12:27:47 +0800

    [Docs] Added documentation for service layer

--- Scooter/Services/ScooterService.cs
+++ Scooter/Services/ScooterService.cs
@@ -44,15 +44,28 @@ namespace Beam_intern.Scooter.Services
             return _repository.Delete(id);
         }
 
+        /**
+         * Domain logic responsible for calculating distance and finding the closest few scooters to a given coordinate.
+         * First, we get all Scooters that's available in the database at the current moment.
+         * Next, we iterate through all the Scooters available and calculate their distance.
+         * If the distance is smaller than the specified distance (termed radius),
+         * the Scooter will be added to a List holding all nearest scooters.
+         * At the same time, their distance and UUID will be saved in a List of Tuples, which will be sorted.
+         * We will then find the UUID of scooters from the shortest distance first and add these Scooters to another
+         * list (called constrainNearestScooters). constrainNearestScooters will then hold a
+         * specific number of closest scooters to the given point and will be returned by this function.
+         */
         public IEnumerable<ScooterDomainModel> GetClosest(ScooterClosestDomainModel scooterClosestDomainModel)
         {
             Coordinates centreCoordinate = scooterClosestDomainModel.CentreCoordinate;
             int nearestNumberOfScooters = scooterClosestDomainModel.NearestNumberOfScooters;
             int radius = scooterClosestDomainModel.Radius;
+            
             IEnumerable<ScooterDomainModel> allScooters = _repository.GetAll().Result;
             List<ScooterDomainModel> allNearestScooters = new List<ScooterDomainModel>();
             List<ScooterDomainModel> constrainNearestScooters = new List<ScooterDomainModel>();
-            List <Tuple<double, Guid>> distanceOfScooters = new List<Tuple<double, Guid>>();
+            List <Tuple<double, Guid>> distanceOfAllNearestScooters = new List<Tuple<double, Guid>>();
+            
             foreach (ScooterDomainModel scooter in allScooters)
             {
                 double distance = CoordinatesDistanceExtensions.GetDistance(
@@ -62,20 +75,20 @@ namespace Beam_intern.Scooter.Services
                     centreCoordinate.Latitude);
                 if (!(distance <= radius)) continue;
                 allNearestScooters.Add(scooter);
-                distanceOfScooters.Add(new Tuple<double, Guid>(distance, scooter.Id));
+                distanceOfAllNearestScooters.Add(new Tuple<double, Guid>(distance, scooter.Id));
             }
 
-            distanceOfScooters.Sort((x,y) => x.Item1.CompareTo(y.Item1));
+            distanceOfAllNearestScooters.Sort((x,y) 
+                                                            => x.Item1.CompareTo(y.Item1));
 
-            while (nearestNumberOfScooters > 0 && distanceOfScooters.Any())
+            while (nearestNumberOfScooters > 0 && distanceOfAllNearestScooters.Any())
             {
-                var initialElement = distanceOfScooters.First();
+                var initialElement = distanceOfAllNearestScooters.First();
                 ScooterDomainModel desiredScooter = allNearestScooters.Find(model => 
                     model.Id.Equals(initialElement.Item2));
                 constrainNearestScooters.Add(desiredScooter);
-                Console.WriteLine("Chosen UUID" + desiredScooter.Id);
                 nearestNumberOfScooters--;
-                distanceOfScooters.Remove(initialElement);
+                distanceOfAllNearestScooters.Remove(initialElement);
             }
             return constrainNearestScooters;
         }
